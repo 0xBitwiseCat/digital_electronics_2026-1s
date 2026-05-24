d06c619d0f7b404e76305e36867be2f57af6137b
ジェイソン <61421639+0xBitwiseCat@users.noreply.github.com>
2026-05-24 15:36:25 -0500

    implement sbox_4bit.v

---
+++ project/sbox_4bit.v
@@ -0,0 +1,30 @@
+module sbox_4bit (
+    input  wire [3:0] data_in,
+    output reg  [3:0] data_out
+);
+
+    // Se sustituyen los valores iniciales por nuevos valores
+    // de acuerdo con la tabla de sustituciones
+    always @(*) begin
+        case (data_in)
+            4'h0: data_out = 4'hC;
+            4'h1: data_out = 4'h5;
+            4'h2: data_out = 4'h6;
+            4'h3: data_out = 4'hB;
+            4'h4: data_out = 4'h9;
+            4'h5: data_out = 4'h0;
+            4'h6: data_out = 4'hA;
+            4'h7: data_out = 4'hD;
+            4'h8: data_out = 4'h3;
+            4'h9: data_out = 4'hE;
+            4'hA: data_out = 4'hF;
+            4'hB: data_out = 4'h8;
+            4'hC: data_out = 4'h4;
+            4'hD: data_out = 4'h7;
+            4'hE: data_out = 4'h1;
+            4'hF: data_out = 4'h2;
+            default: data_out = 4'h0; // Caso por defecto de seguridad
+        endcase
+    end
+
+endmodule
